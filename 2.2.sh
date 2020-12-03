c5f90891138d59877931f9ce986374001a00489f
AnotherTest <ali.mpfard@gmail.com>
2020-12-03 18:53:40 +0330

    don't use sed :)

--- 2.2.sh
+++ 2.2.sh
@@ -2,8 +2,7 @@ valid=0
 for $(cat 2.input) {
   match $it {
     "*-* ?: *" as (min max letter passw) {
-      replaced=$(echo -n "${substring $(expr $min - 1) 1 $passw}${substring $(expr $max - 1) 1 $passw}" \
-        | sed -e "s/[^$letter]//g")
+      replaced=${regexReplace "[^$letter]" '' "${substring $(expr $min - 1) 1 $passw}${substring $(expr $max - 1) 1 $passw}"}
       if test ${length $replaced} -eq 1 {
         valid=$(expr $valid + 1)
       }
--- 2.sh
+++ 2.sh
@@ -1,9 +1,9 @@
 valid=0
-for $(cat foo) {
+for $(cat 2.input) {
     match $it {
         "6-15 n*" { valid=$(expr $valid + 1) }
         "*-* ?: *" as (min max letter passw) {
-            replaced=$(echo -n $passw | sed -e "s/[^$letter]//g")
+            replaced=${regexReplace "[^$letter]" '' $passw}
             count=${length $replaced}
             if test \( $count -le $max \) -a \( $count -ge $min \) {
                 valid=$(expr $valid + 1)
